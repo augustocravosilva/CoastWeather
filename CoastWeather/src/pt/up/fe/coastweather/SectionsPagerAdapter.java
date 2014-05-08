feb61b27eff96b313bdc00584d46cc53f938e716
Augusto Silva <ei11113@fe.up.pt>
2014-05-08 15:40:09 +0100

    cenas

---
+++ CoastWeather/src/pt/up/fe/coastweather/SectionsPagerAdapter.java
@@ -0,0 +1,75 @@
+package pt.up.fe.coastweather;
+
+import java.util.Locale;
+
+import pt.up.fe.coastweather.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+	private Context context;
+
+	public SectionsPagerAdapter(FragmentManager fm, Context context) {
+		super(fm);
+		this.context = context;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		// getItem is called to instantiate the fragment for the given page.
+		// Return a DummySectionFragment (defined as a static inner class
+		// below) with the page number as its lone argument.
+		/*Fragment fragment = new DummySectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+		fragment.setArguments(args);
+		return fragment;*/
+
+		if(position == 3) {
+			AddReviewFragment fragment = new AddReviewFragment();
+			Bundle args = new Bundle();
+			args.putInt(AddReviewFragment.ARG_SECTION_NUMBER, position + 1);
+			fragment.setArguments(args);
+			return fragment;
+		}
+		else {
+			Fragment fragment = new DummySectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+			fragment.setArguments(args);
+			return fragment;
+		}
+	}
+
+	@Override
+	public int getCount() {
+		// Show 3 total pages.
+		return 4;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return context.getString(R.string.title_section1).toUpperCase(l);
+		case 1:
+			return context.getString(R.string.title_section2).toUpperCase(l);
+		case 2:
+			return context.getString(R.string.title_section3).toUpperCase(l);
+		case 3:
+			return context.getString(R.string.title_section4).toUpperCase(l);
+		}
+		return null;
+	}
+}
