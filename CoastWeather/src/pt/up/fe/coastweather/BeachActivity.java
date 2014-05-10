bf90a451ec4106f8aa287092c103b0141b509d9f
Tiago Fernandes <ei11054@fe.up.pt>
2014-05-10 22:10:37 +0100

    working on beach activity

--- CoastWeather/src/pt/up/fe/coastweather/BeachActivity.java
+++ CoastWeather/src/pt/up/fe/coastweather/BeachActivity.java
@@ -5,20 +5,35 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BeachActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setTitle("Praia da Rocha");
-		
+
 		setContentView(R.layout.activity_beach);
-		
-		new DownloadImageTask((ImageView) findViewById(R.id.icon_beach))
-		.execute("https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-ash2/t1.0-9/523837_507942599227785_89321393_n.jpg");
+
+		new DownloadImageTask((ImageView) findViewById(R.id.icon_beach_beach))
+		.execute("http://c4.quickcachr.fotos.sapo.pt/i/of10410e3/6755316_Zgf0o.jpeg");
+
+		ImageView image_feeling = (ImageView) findViewById(R.id.icon_beach_beach);
+		ImageView image_weather1 = (ImageView) findViewById(R.id.icon_beach_weather1);
+		ImageView image_weather2 = (ImageView) findViewById(R.id.icon_beach_weather2);
+		ImageView image_flag = (ImageView) findViewById(R.id.icon_beach_flag);
+		TextView timeView = (TextView) findViewById(R.id.beach_time);
+
+		timeView.setText("5 minutes ago");      
+		image_feeling.setImageResource(R.drawable.ic_feeling_2);
+		image_weather1.setImageResource(R.drawable.ic_weather_sunny);
+		image_weather2.setImageResource(R.drawable.ic_weather_windy);
+		image_flag.setImageResource(R.drawable.ic_flag_green);
+
+
 	}
 
 	@Override
